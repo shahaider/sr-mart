35d243d42c31a6800836b6903b612cd8d049f2d1
Shahrukh <shahaider@gmail.com>
2017-06-20 12:31:22 +0500

    first repos

---
+++ sr-mart/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sr-mart
+//
+//  Created by Syed Shahrukh Haider on 20/06/2017.
+//  Copyright © 2017 Syed Shahrukh Haider. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
